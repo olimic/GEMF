67ee51efdf96af095d68e0860fc1b976ac40ebbc
olimic <olimicdd@gmail.com>
2015-08-30 12:23:11 +0200

    add GEMFCreator

---
+++ GEMFCreator/GEMFCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GEMF;
+
+namespace GEMFCreator
+{
+    public class GEMFCreator
+    {
+        public GEMFCreator()
+        {
+
+        }
+
+        public void Create(string outputfile, string inputpath, int maxzoom)
+        {
+
+        }
+    }
+}
---
+++ GEMFCreator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GEMFCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("GEMFCreator output.gemf input-path [max-zoom]");
+            }
+            else
+            {
+                try
+                {
+                    GEMFCreator cr = new GEMFCreator();
+                    cr.Create(args[0], args[1], args.Length > 2 ? Convert.ToInt32(args[2]) : 20);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
