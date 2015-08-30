836349f9c662f3d97b1ff7af4326b291c803a981
olimic <olimicdd@gmail.com>
2015-08-30 10:51:31 +0200

    initial version

---
+++ GEMF/Range.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GEMF
+{
+    public class Range
+    {
+        public UInt32 Zoom;
+        public UInt32 XMin;
+        public UInt32 XMax;
+        public UInt32 YMin;
+        public UInt32 YMax;
+        public UInt32 SourceIndex;
+        public UInt64 Offset;
+
+        public bool IsInRange(uint zoom, uint x, uint y)
+        {
+            if (Zoom != zoom)
+                return false;
+            if (XMin <= x && XMax >= x &&
+                YMin <= y && YMax >= y)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ GEMFExporter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GEMFExporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("GEMFExporter input.gemf output-path image-extension(png/jpg)");
+            }
+            else
+            {
+                try
+                {
+                    GEMFExporter exp = new GEMFExporter();
+                    exp.Export(args[0], args[1], args[2]);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
